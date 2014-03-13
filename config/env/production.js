95814cdbe01ecd2f406e5a44fb7194152a513524
Jacob Walker <jacob@tortugas-llc.com>
2014-03-13 11:24:42 -0500

    Add branding to differentiate from the MEAN stack.

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,7 @@
 module.exports = {
     db: 'mongodb://localhost/mean',
     app: {
-        name: 'MEAN - A Modern Stack - Production'
+        name: 'Startup Wichita'
     },
     facebook: {
         clientID: 'APP_ID',
--- config/env/test.js
+++ config/env/test.js
@@ -4,7 +4,7 @@ module.exports = {
     db: 'mongodb://localhost/mean-test',
     port: 3001,
     app: {
-        name: 'MEAN - A Modern Stack - Test'
+        name: 'Startup Wichita -- Test'
     },
     facebook: {
         clientID: 'APP_ID',
