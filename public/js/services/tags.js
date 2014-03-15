5269672d23c5a73b7d2c148d4d84edde87666079
Charles Haines <chaines@codemonkeyjava.com>
2014-03-15 12:11:31 -0500

    Issue 10: Create Tag Service for Angular
    Signed-off-by: Charles Haines <chaines@codemonkeyjava.com>

---
+++ public/js/services/tags.js
@@ -0,0 +1,23 @@
+(function(angular) {
+    'use strict';
+
+    // Tags service used for articles REST endpoint
+    var TagsService = [
+        '$resource',
+        function($resource) {
+            return $resource('/api/v1/tags/:tagId', {
+                tagsId: '@_id'
+            }, {
+                update: {
+                    method: 'PUT'
+                },
+                search: {
+                    method: 'GET',
+                    isArray: true
+                }
+            });
+        }
+    ];
+
+    angular.module('startupwichita.services').factory('Tags', TagsService);
+})(window.angular);
---
+++ test/karma/unit/services/tags.spec.js
@@ -0,0 +1,45 @@
+(function() {
+    'use strict';
+
+    // Tags Controller Spec
+    describe('startupwichita services', function () {
+        // Load the startupwichita module
+        beforeEach(module('startupwichita'));
+
+        describe('tagsService', function () {
+            it ('should issue PUT request to /api/v1/tags1', inject(function(Tags, $httpBackend) {
+                var updateTag,
+                    updateTimestamp = Date.now();
+
+                $httpBackend.when('PUT', '/api/v1/tags').respond({id: '507f1f77bcf86cd799439011', name: 'Tag 1', updated: updateTimestamp});
+
+                updateTag = new Tags({
+                    id: '507f1f77bcf86cd799439011',
+                    name: 'Tag 1'
+                });
+
+                updateTag.$update();
+
+                $httpBackend.flush();
+                expect(updateTag.id).toEqual('507f1f77bcf86cd799439011');
+                expect(updateTag.name).toEqual('Tag 1');
+                expect(updateTag.updated).toEqual(updateTimestamp);
+            }));
+        });
+
+        describe('tagsService', function () {
+            it ('should issue GET request to /api/v1/tags?search=tag', inject(function(Tags, $httpBackend) {
+                $httpBackend.when('GET', '/api/v1/tags?search=tag').respond([
+                    {id: '507f1f77bcf86cd799439011', name: 'Tag 1'},
+                    {id: '507f1f77bcf86cd799439012', name: 'Tag 2'}
+                ]);
+
+                var tag = Tags.search({search: 'tag'});
+
+                $httpBackend.flush();
+                expect(tag[0].id).toEqual('507f1f77bcf86cd799439011');
+                expect(tag[0].name).toEqual('Tag 1');
+            }));
+        });
+    });
+})();
