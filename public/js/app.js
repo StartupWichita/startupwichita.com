07e9ce4523e1ab7003a2c5b008878ae6e0810224
Seth Etter <sethetter@gmail.com>
2014-05-18 12:34:12 -0500

    Partition filter to split resources into rows
    Conflicts:
    	public/js/filters.js

--- public/js/app.js
+++ public/js/app.js
@@ -9,10 +9,12 @@
         'ui.router',
         'startupwichita.services',
         'startupwichita.controllers',
-        'startupwichita.directives'
+        'startupwichita.directives',
+        'startupwichita.filters'
     ]);
 
     angular.module('startupwichita.services', []);
     angular.module('startupwichita.controllers', []);
     angular.module('startupwichita.directives', []);
+    angular.module('startupwichita.filters', []);
 })(window.angular);
