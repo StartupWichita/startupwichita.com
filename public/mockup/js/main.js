f1126fc8704802b790e0d53f72b87fad9d7c4709
Jacob Walker <jacob@tortugas-llc.com>
2014-05-29 08:35:49 -0500

    Remove mockup before merging to master

--- public/mockup/js/main.js
+++
@@ -1,8 +0,0 @@
-var stickyEl= document.getElementById('stickyBar')
-window.addEventListener('scroll', function() {
-  var topBuffer = document.getElementById('logoTop').offsetHeight; 
-
-  window.pageYOffset > topBuffer ?
-    stickyEl.classList.add('navbar-fixed-top') : 
-    stickyEl.classList.remove('navbar-fixed-top');
-});
