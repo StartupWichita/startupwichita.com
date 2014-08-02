daa1b7468c19b35e92844ffeb4ba150126edc9a1
Chuck Haines <chaines@codemonkeyjava.com>
2014-08-02 11:48:39 -0500

    Fix issue with latest puppet and ruby 1.9

--- vm/shell/prereqs.sh
+++ vm/shell/prereqs.sh
@@ -34,6 +34,27 @@ else
   echo 'git found.'
 fi
 
+$(which ruby1.9.1 < /dev/null 2>&1)
+FOUND_RUBY19UND_RUBY19=$?
+if ["$FOUND_RUBY19" -ne '0']; then
+  sudo apt-get -q -y install ruby1.9.1 ruby1.9.1-dev \
+    rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
+    build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev
+
+  sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
+         --slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz \
+                        /usr/share/man/man1/ruby1.9.1.1.gz \
+        --slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
+        --slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
+        --slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
+
+  # choose your interpreter
+  # changes symlinks for /usr/bin/ruby , /usr/bin/gem
+  # /usr/bin/irb, /usr/bin/ri and man (1) ruby
+  sudo update-alternatives --config ruby
+  sudo update-alternatives --config gem
+fi
+
 if [ ! -d "$PUPPET_DIR" ]; then
   mkdir -p $PUPPET_DIR
 fi
