90b8410bebe58a836f80efbd5403808055ebb0d2
Matthew M. Keeler <keelerm84@gmail.com>
2014-10-30 16:29:02 -0400

    Rail scaffolding for resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :resources
+
   resources :news
 
   resources :events
---
+++ db/migrate/20141030202831_create_resources.rb
@@ -0,0 +1,6 @@
+class CreateResources < ActiveRecord::Migration
+  def change
+    create_table :resources do |t|
+    end
+  end
+end
