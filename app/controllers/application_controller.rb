89355a3c9bd0d2e54ef18f372bd5ed654eb357bd
Matthew M. Keeler <keelerm84@gmail.com>
2015-01-01 16:30:33 -0500

    Hide utility links for non-authorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :can_edit?, :can_edit?
+
   protected
 
   def configure_permitted_parameters
