418267740368940053b71b362a578eb3f9af0fab
Barry Welch <btwelchy@gmail.com>
2015-02-14 22:48:03 -0600

    People by common skills interests

--- app/models/person.rb
+++ app/models/person.rb
@@ -22,4 +22,12 @@ class Person < ActiveRecord::Base
     medium: '300x300>'
   }
   do_not_validate_attachment_file_type :avatar
+
+  def self.all_skill_tags
+    ActsAsTaggableOn::Tagging.all.where(context: "skills").map {|tagging| { "id" => "#{tagging.tag.id}", "name" => tagging.tag.name, "tagging_count" => tagging.tag.taggings_count } }.select{|t| t['tagging_count'] > 1}.uniq
+  end
+
+  def self.all_interest_tags
+    ActsAsTaggableOn::Tagging.all.where(context: "interests").map {|tagging| { "id" => "#{tagging.tag.id}", "name" => tagging.tag.name, "tagging_count" => tagging.tag.taggings_count } }.select{|t| t['tagging_count'] > 1}.uniq
+  end
 end
