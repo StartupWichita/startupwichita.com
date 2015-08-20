3444f9a1d4cdac2cd4348da9ae4bd3e897faf362
kentonh <kenton@gobananago.com>
2015-08-19 22:29:35 -0500

    atempting to fix heroku precomplie error 3

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
+config.assets.initialize_on_precompile = false
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,7 +10,6 @@ Bundler.require(*Rails.groups)
 
 module StartupwichitaCom
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
