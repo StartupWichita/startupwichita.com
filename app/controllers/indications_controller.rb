f9406e231a2225802c6e3d75d6ef3e69d11bba11
Alex Timofeev <atimofeev@reactant.ru>
2017-10-01 20:29:21 +0700

    Implement indication action and pass resource attributes

--- app/controllers/indications_controller.rb
+++ app/controllers/indications_controller.rb
@@ -1,6 +1,10 @@
 class IndicationsController < ApplicationController
+  INDICATION_TYPES = %i(event news resource)
   before_filter :authenticate_user!
 
   def create
+    return unless INDICATION_TYPES.include?(params[:type].to_sym)
+    params[:type].capitalize.constantize.find(params[:id]).indications.create(user_id: current_user.id)
+    redirect_to :back, notice: 'Thank you, admins has been notified!'
   end
 end
