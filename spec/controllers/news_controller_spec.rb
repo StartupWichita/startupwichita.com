6718c9ee7ee5dc34e3703a1203b18b6c48a4f7f9
Frank DelPidio <frankdelpidio@gmail.com>
2017-10-07 00:15:04 -0400

    Make news_controller tests pass

--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -8,32 +8,56 @@ describe NewsController, type: :controller do
   end
 
   context "authenticated" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:news) { FactoryGirl.create(:news) }
-
-    before(:each) do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in user
-    end
-
     describe "#create" do
       context "success" do
         it "creates a News record" do
+          user = build(:user)
+          sign_in user
+          count = News.count
+
           post :create, news: FactoryGirl.attributes_for(:news)
 
-          expect(assigns(:news)).to_not be_a_new(News)
+          expect(News.count).to eq(count + 1)
+          expect(assigns(:news).user).to eq(user)
+          expect(response).to redirect_to(news_path assigns(:news).id)
         end
       end
     end
 
     describe "#destroy" do
-      let(:news) { FactoryGirl.create(:news) }
-
       context "success" do
-        it "succeeds" do
+        it "allows admin to destroy news" do
+          admin = build(:user, admin: true)
+          news = create(:news)
+          sign_in(admin)
+
           delete :destroy, id: news.id
 
           expect(response).to redirect_to(news_index_path)
+        end
+
+        it "it allows user to delete their own news" do
+          user = build(:user, admin: false)
+          news = create(:news, user: user)
+          sign_in(user)
+
+          delete :destroy, id: news.id
+
+          expect(response).to redirect_to(news_index_path)
+
+        end
+      end
+
+      context "failure" do
+        it "prevents user from deleting news that isn't theirs" do
+          user_1 = build(:user, admin: false)
+          user_2 = build(:user, admin: false)
+          news = create(:news, user: user_2)
+          sign_in(user_1)
+
+          delete :destroy, id: news.id
+
+          expect(response).to redirect_to(news_path(news))
         end
       end
     end
