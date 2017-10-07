89fe93c034d377916b7f4808c513021905c91e87
Frank DelPidio <frankdelpidio@gmail.com>
2017-10-06 23:23:19 -0400

    Create passing event_controller destroy tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -6,25 +6,31 @@ describe EventsController, type: :controller do
   context "unauthenticated" do
     describe "#index" do
       it "shows a list of current and future Events" do
-        expect(Event).to receive(:coming_soon)
-
+        expect(Event).to receive(:upcoming)
         get :index
+      end
+    end
+
+    describe "#destroy" do
+      it "prevents people not signed in from deleting events" do
+        event = create(:event)
+
+        delete :destroy, id: event.id
+
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
 
   context "authenticated" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:event) { FactoryGirl.create(:event) }
-
-    before(:each) do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in user
-    end
-
     describe "#create" do
       context "success" do
         it "creates an Event" do
+          user = build(:user)
+          event = create(:event)
+          request.env["devise.mapping"] = Devise.mappings[:user]
+          sign_in user
+
           post :create, event: FactoryGirl.attributes_for(:event)
 
           expect(assigns(:event)).to_not be_a_new(Event)
@@ -33,13 +39,39 @@ describe EventsController, type: :controller do
     end
 
     describe "#destroy" do
-      let(:event) { FactoryGirl.create(:event) }
-
       context "success" do
-        it "succeeds" do
+        it "allows admin to destroy event" do
+          admin = build(:user, admin: true)
+          event = create(:event)
+          sign_in(admin)
+
           delete :destroy, id: event.id
 
           expect(response).to redirect_to(events_path)
+        end
+
+        it "it allows user to delete their own event" do
+          user = build(:user, admin: false)
+          event = create(:event, user: user)
+          sign_in(user)
+
+          delete :destroy, id: event.id
+
+          expect(response).to redirect_to(events_path)
+
+        end
+      end
+
+      context "failure" do
+        it "prevents user from deleting an event that isn't theirs" do
+          user_1 = build(:user, admin: false)
+          user_2 = build(:user, admin: false)
+          event = create(:event, user: user_2)
+          sign_in(user_1)
+
+          delete :destroy, id: event.id
+
+          expect(response).to redirect_to(event_path(event))
         end
       end
     end
