9993b0389e89ce1dd5c5a44c99ba9b6958f206d1
Frank DelPidio <frankdelpidio@gmail.com>
2017-10-07 00:40:06 -0400

    Make resources_controller tests pass

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -8,34 +8,58 @@ describe ResourcesController, type: :controller do
   end
 
   context "authenticated" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:resource) { FactoryGirl.create(:resource) }
-
-    before(:each) do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in user
-    end
-
     describe "#create" do
       context "success" do
         it "creates a Resource record" do
+          user = build(:user)
+          sign_in user
+          count = Resource.count
+
           post :create, resource: FactoryGirl.attributes_for(:resource)
 
-          expect(assigns(:resource)).to_not be_a_new(Resource)
+          expect(Resource.count).to eq(count + 1)
+          expect(assigns(:resource).user).to eq(user)
+          expect(response).to redirect_to(resource_path assigns(:resource).id)
         end
       end
     end
 
-    describe "#destroy" do
-      let(:resource) { FactoryGirl.create(:resource) }
+   describe "#destroy" do
+         context "success" do
+           it "allows admin to destroy resource" do
+             admin = build(:user, admin: true)
+             resource = create(:resource)
+             sign_in(admin)
 
-      context "success" do
-        it "succeeds" do
-          delete :destroy, id: resource.id
+             delete :destroy, id: resource.id
 
-          expect(response).to redirect_to(resources_path)
-        end
-      end
-    end
+             expect(response).to redirect_to(resources_path)
+           end
+
+           it "it allows user to delete their own resource" do
+             user = build(:user, admin: false)
+             resource = create(:resource, user: user)
+             sign_in(user)
+
+             delete :destroy, id: resource.id
+
+             expect(response).to redirect_to(resources_path)
+
+           end
+         end
+
+         context "failure" do
+           it "prevent user from deleting a resource that isn't theirs" do
+             user_1 = build(:user, admin: false)
+             user_2 = build(:user, admin: false)
+             resource = create(:resource, user: user_2)
+             sign_in(user_1)
+
+             delete :destroy, id: resource.id
+
+             expect(response).to redirect_to(resource_path(resource))
+           end
+         end
+       end
   end
 end
