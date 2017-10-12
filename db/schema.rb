1619057a3029b47a0cf73b5dd23854c32542a40d
Francisco Quintero <frajaquico@yahoo.es>
2017-10-12 12:36:29 -0500

    Add Company model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001073059) do
+ActiveRecord::Schema.define(version: 20171012172356) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.string   "logo"
+    t.string   "website"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.datetime "starts_at"
