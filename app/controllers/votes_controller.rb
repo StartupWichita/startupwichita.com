c8ac8004bc29db77d0843caa2de5b0ddba464ddc
Sarah Vessels <cheshire137@gmail.com>
2017-10-14 17:38:42 -0500

    Flesh out vote create endpoint

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,20 @@
 class VotesController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
+    votable = if params[:event_id]
+      Event.find(params[:event_id])
+    end
+    return head(:not_found) unless votable
+    return head(:forbidden) if can_edit?(votable)
+
+    if params.key?(:upvote)
+      votable.liked_by current_user
+    else
+      votable.disliked_by current_user
+    end
+
+    head :created
   end
 
   def destroy
